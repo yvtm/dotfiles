db49e7d563f32de92181ba04415bad84d1a05791
yvtm <yvtm@posteo.net>
2025-01-25 17:12:01 -0800

    test

--- configuration.nix
+++ configuration.nix
@@ -15,6 +15,10 @@
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.grub.enable = true;
+  boot.loader.grub.devices = [ "nodev" ];
+  boot.loader.grub.efiSupport = true;
+  boot.loader.grub.useOSProber = true;
 boot.kernelPackages = pkgs.linuxPackages_latest;
 
 hardware.bluetooth.enable=true;
